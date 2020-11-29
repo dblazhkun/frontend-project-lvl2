b9bf1e4a006f50be5b1d9777aebf6377e20022b6
Denis Blazhkun <denisblazhkun@gmail.com>
2020-11-29 21:32:05 +0300

    add --format option

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
-import commander from 'commander';
 import fs from 'fs';
+import commander from 'commander';
 
 const { version } = JSON.parse(fs.readFileSync('package.json', 'utf8'))
-
 const program = new commander.Command();
 
-program.version(version)
+program
+  .version(version)
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+
+program.option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
+
+if (program.format) console.log(`- ${program.format}`);
